3dec2382915110eeee3afab47c13123667f20fec
vhariths <vedu.hariths@nsquaredglobal.com>
2011-05-07 22:24:22 -0400

    Added relations etc

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,6 @@
 class Micropost < ActiveRecord::Base
+	belongs_to :user, :class_name=>'Micropost'
+	
+	validates :content, :length =>{:maximum =>140}
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :microposts
 	validates_presence_of :name, :on => :create, :message => "can't be blank"
 	validates_presence_of :email_add, :on => :create, :message => "can't be blank"
 	validates_format_of :email_add, :with => /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$/, :on => :create, :message => "is invalid"
